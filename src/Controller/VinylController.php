bbff17751c0ded50614cd3dd6b4d3fdf007df3b8
Aziz zgolli <zgolliaziz206@gmail.com>
2024-04-16 13:49:26 +0100

    Aziz Zgolli : Partie 1

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Twig\Environment;
+use function Symfony\Component\String\u;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+
+class VinylController extends AbstractController
+{
+    #[Route('/', name: 'app_homepage')]
+    function homepage(): Response
+    {
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
+        ]);
+    }
+    #[Route('/browse/{slug}', name: 'app_browse')]
+    function browse(string $slug = null): Response
+    {
+        $genre = $slug ? u(str_replace("-", " ", $slug))->title(true) : null;
+
+        return $this->render('vinyl/browse.html.twig', [
+            'genre' => $genre,
+        ]);
+    }
+}
